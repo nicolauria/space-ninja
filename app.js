b8fc8ecbc8b60d0f92fecdbd07c8be543a0d41ad
nico <nicolauria@outlook.com>
2018-11-02 11:49:46 -0700

    Revert "comment out app server"
    This reverts commit 4467daa9496817a9519359ee18edd4492b7b6e16.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// const express = require('express')
-// const app = express()
-// const path = require('path')
-// const PORT = process.env.PORT || 3000;
-//
-// app.listen(PORT, () => {
-//   console.log(__dirname);
-//   console.log(`listening on ${PORT}`)
-// })
-//
-// app.get('/', (request, res) => {
-//   res.sendFile(path.join(__dirname, './index.html'))
-// })
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(__dirname);
+  console.log(`listening on ${PORT}`)
+})
+
+app.get('/', (request, res) => {
+  res.sendFile(path.join(__dirname, './index.html'))
+})
